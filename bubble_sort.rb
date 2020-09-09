23fd2f3cf42d34033f96652a6bb89b95c4997f2a
Jyoti <jyotikiran12@gmail.com>
2020-09-09 19:56:17 +0530

    Fix rubocop errors.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,42 +1,39 @@
 # bubble_sort method
 def bubble_sort(array)
-    len=array.length
-    for i in (0...len-1)
-        for j in (0...len-1-i) do
-            if array[j] > array[j+1]
-                array[j], array[j+1] = array[j+1], array[j]
-            end
-        end
+  len = array.length
+  (0...len - 1).each do |i|
+    (0...len - 1 - i).each do |j|
+      array[j], array[j + 1] = array[j + 1], array[j] if array[j] > array[j + 1]
     end
+  end
 end
 
-
-#bubble_sort_by method
+# bubble_sort_by method
 def bubble_sort_by(array)
-    len = array.length - 1
-    swapped = true
-    while swapped do
-        swapped = false
-        (0..len-1).each do |i|
-            if yield(array[i], array[i+1]) > 0
-                swap = array[i+1]
-                array[i+1] = array[i]
-                array[i] = swap
-                swapped = true
-            end
-        end
-        len -= 1
+  len = array.length - 1
+  swapped = true
+  while swapped
+    swapped = false
+    (0..len - 1).each do |i|
+      next unless yield(array[i], array[i + 1]).positive?
+
+      swap = array[i + 1]
+      array[i + 1] = array[i]
+      array[i] = swap
+      swapped = true
     end
-    p array
+    len -= 1
+  end
+  p array
 end
 
 # Running the above two methods below
 
-test = [89,4,2,7,9,4,0,3,-1]
+test = [89, 4, 2, 7, 9, 4, 0, 3, -1]
 bubble_sort(test)
 p test
 
-array_word = ["hi","hello","hey","hat","hig","hi","h","cat","JackJill","joy"]
+array_word = %w[hi hello hey hola game]
 bubble_sort_by(array_word) do |left, right|
-    left.length - right.length
+  left.length - right.length
 end
