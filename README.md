05f8d84aef56c399ecfe098a9673e84c5e5b11c8
Jyoti <jyotikiran12@gmail.com>
2020-09-09 19:23:15 +0530

    Add #bubble_sort_by method and updated the README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# bubble-sort
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Advanced Building Blocks - Bubble Sort
+
+> The aim of this project is to implement the bubble sort algorithm. Assignment is focused on the use of yield and passing a block to a method.
+
+## Built With
+
+- Ruby
+- Rubocop
+
+## Authors
+
+👤 **Author1**
+
+- GitHub: [@jrai0792](https://github.com/githubhandle)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,40 @@
+# bubble_sort method
 def bubble_sort(array)
     len=array.length
     for i in (0...len-1)
-        for j in (0...len-1) do
+        for j in (0...len-1-i) do
             if array[j] > array[j+1]
                 array[j], array[j+1] = array[j+1], array[j]
             end
         end
     end
 end
+
+
+#bubble_sort_by method
+def bubble_sort_by(array)
+    len = array.length - 1
+    swapped = true
+    while swapped do
+        swapped = false
+        (0..len-1).each do |i|
+            if yield(array[i], array[i+1]) > 0
+                swap = array[i+1]
+                array[i+1] = array[i]
+                array[i] = swap
+                swapped = true
+            end
+        end
+        len -= 1
+    end
+    p array
+end
+
 test = [89,4,2,7,9,4,0,3,-1]
 bubble_sort(test)
 p test
+
+array_word = ["hi","hello","hey","hat","hig","hi","h","cat","JackJill","joy"]
+bubble_sort_by(array_word) do |left, right|
+    left.length - right.length
+end
